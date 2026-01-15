360c18902968324eb9ce2236b56a8ee20034027e
Shreyas Nikte <subscribe.s102938@gmail.com>
2026-01-15 18:24:56 +0100

    Remove Chonky file browser dependencies and associated styles from the frontend codebase

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,12 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { setChonkyDefaults } from 'chonky'
-import { ChonkyIconFA } from 'chonky-icon-fontawesome'
 import App from './App'
 import './index.css'
-
-// Set Chonky defaults (icon component)
-setChonkyDefaults({ iconComponent: ChonkyIconFA })
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
