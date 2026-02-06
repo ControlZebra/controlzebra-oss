15659d99ce6ff56aa9b90ed7bd94ea2a4985e560
shreyasnikte <shreyasnikte@users.noreply.github.com>
2026-02-06 08:55:11 +0100

    feat(viewers): enhance L5XViewer with multi-tab support and new data type display

---
+++ frontend/src/components/viewers/l5x/index.ts
@@ -0,0 +1,6 @@
+/**
+ * L5X Viewer internal components
+ */
+export { TabBar } from './TabBar';
+export { useTabs, type Tab, type TabData, type TabType, generateTabId } from './useTabs';
+export { DataTypeTable } from './DataTypeTable';
