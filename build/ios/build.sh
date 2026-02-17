be603dda8e7b242e16515c4b82edb89865725e34
nikteworks <43822106+nikteworks@users.noreply.github.com>
2026-02-17 14:41:15 +0100

    feat: Update application version to v0.8.0-beta across all configuration files

--- build/ios/build.sh
+++ build/ios/build.sh
@@ -4,8 +4,8 @@ set -e
 # Build configuration
 APP_NAME="control-zebra"
 BUNDLE_ID="com.controlzebra.controlzebra"
-VERSION="v0.7.0-beta"
-BUILD_NUMBER="v0.7.0-beta"
+VERSION="v0.8.0-beta"
+BUILD_NUMBER="v0.8.0-beta"
 BUILD_DIR="build/ios"
 TARGET="simulator"
 
