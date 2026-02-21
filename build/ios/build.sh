71ca558b6759e70551df80d7e23a80d0722db123
shreyasnikte <shreyasnikte@users.noreply.github.com>
2026-02-21 20:33:24 +0100

    feat: Update application version to v0.13.0-beta across all configuration files and add integration plan for Logix VCS Custom Tools Sidecar

--- build/ios/build.sh
+++ build/ios/build.sh
@@ -4,8 +4,8 @@ set -e
 # Build configuration
 APP_NAME="control-zebra"
 BUNDLE_ID="com.controlzebra.controlzebra"
-VERSION="v0.12.0-beta"
-BUILD_NUMBER="v0.12.0-beta"
+VERSION="v0.13.0-beta"
+BUILD_NUMBER="v0.13.0-beta"
 BUILD_DIR="build/ios"
 TARGET="simulator"
 
